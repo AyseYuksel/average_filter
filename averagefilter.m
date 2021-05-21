0c2585ad4bb233a21e7cadd8385bfaf965974432
AyseYuksel <68689527+AyseYuksel@users.noreply.github.com>
2021-05-21 08:39:48 -0700

    initial commit

---
+++ averagefilter.m
@@ -0,0 +1,23 @@
+﻿picture=imread('coins.jpg');
+graypic=rgb2gray(picture);
+
+
+%7x7 average mask
+mask=ones(7);
+maskK=49;
+
+%the difference is the value in the middle of the matrix
+difference=(7-1)/2;
+
+copy=graypic;
+
+%noisepic size -> 247x495
+
+for i=(difference+1):(247-difference-1)
+    for j=(difference+1):(495-difference-1)
+        %ROI=region of interest
+        ROI=graypic(i-difference:i+difference,j-difference:j+difference);
+        copy(i,j)=sum(sum(ROI.*mask))/maskK;
+    end
+end
+imshow(copy)
